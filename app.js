b623bb291d3a212a5ab37cd42c9157690d3ae7fb
Luã Freire <riquelmefreire_@hotmail.com>
2025-02-09 17:07:25 -0300

    Projeto quase  concluido

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionar(id) {
+    //recuperar o valor nome do amigo
+    let amigo = document.getElementById('nome-amigo').value;
+
+    //adicionar ele na lista de amigos incluidos.
+    let lista = document.getElementById('lista-amigos');
+
+    if(lista.textContent == '') {
+        lista.textContent = amigo;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo;
+    }
+
+    document.getElementById('nome-amigo').value = '';
+}
